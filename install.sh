bf64d1125633a0e9d73b55f596f96b54b46279d7
Simon Whittemore <simon.whittemore@gmail.com>
2022-08-16 17:49:59 +0100

    gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mv ~/.tmux.conf ~/._tmux.conf
 mv ~/.bash_profile ~/._bash_profile
-mv ~/.gitconfig ~/.gitconfig
+mv ~/.gitconfig ~/._gitconfig
 
 ln -vfns "~/dotfiles/.tmux.conf" ~
 ln -vfns "~/dotfiles/.bash_profile" ~
